6842fa778f9e63f98582d6fb85e2cdd8ee076b09
AhLei <138641969+9Kittipich@users.noreply.github.com>
2025-03-05 12:54:06 +0700

    check mudule #15

---
+++ src/modules/mdul_counterloanfast/app/pages/counterloanfast/[id]/scan.vue
@@ -0,0 +1,12 @@
+<template>
+  <RealmPageEach :pageId></RealmPageEach>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  name: `counterloanfast.each.scan`,
+});
+const pageId = {
+  page: 'scan',
+};
+</script>
