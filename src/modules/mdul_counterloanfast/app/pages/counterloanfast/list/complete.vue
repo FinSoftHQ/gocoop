c51458b23d75f297fa038e0881dd2b06c9040ad0
Jane1m <rinda@finsoft2023.com>
2025-03-06 22:03:46 +0700

    Refactor name field to fname and enhance EntityTable with fullname and age display in counterloanfast and counterregister modules edit-column #38

--- src/modules/mdul_counterloanfast/app/pages/counterloanfast/list/complete.vue
+++ src/modules/mdul_counterloanfast/app/pages/counterloanfast/list/complete.vue
@@ -1,5 +1,19 @@
 <template>
-  <RealmPageList :pageId></RealmPageList>
+  <RealmPageList :pageId>
+    <template #default="{ wrapped, entries, resolver }">
+      <EntityTable
+        :data="wrapped.data"
+        :columns="columns"
+        :entries="entries"
+        :resolver="resolver"
+        @selectionChanged="select"
+      >
+        <template #fullname-data="{ row, column }">
+          {{ getPrefix(row.prefix) }}{{ row.fname }} {{ row.lname }}
+        </template>
+      </EntityTable>
+    </template>
+  </RealmPageList>
 </template>
 
 <script setup lang="ts">
@@ -9,4 +23,54 @@ definePageMeta({
 const pageId = {
   page: 'complete',
 };
+
+const pageDef = usePageDefinition(pageId);
+const pageFunctions = usePageFunctions(pageDef);
+
+const columns = [
+  {
+    key: 'no',
+    label: 'ลำดับ',
+  },
+  {
+    key: 'createdAt',
+    label: 'เขียนเมื่อวันที่',
+    sortable: true
+  },
+  {
+    key: 'fullname',
+    label: 'ชื่อ-สกุล',
+  }, {
+    key: 'position',
+    label: 'ตำแหน่ง',
+  }, {
+    key: 'department',
+    label: 'สังกัด',
+  }, {
+    key: 'bureau',
+    label: 'หน่วยงาน',
+  }, {
+    key: 'amount',
+    label: 'ยอดที่ต้องการกู้ (บาท)',
+  },
+]
+
+const prefix: { [key: string]: string } = {
+  option1: 'นาย',
+  option2: 'นาง',
+  option3: 'นางสาว',
+  option4: 'เด็กชาย',
+  option5: 'เด็กหญิง',
+  option6: 'ดอกเตอร์',
+  option7: 'แพทย์ชาย',
+  option8: 'แพทย์หญิง',
+};
+
+function getPrefix(key: string): string {
+  return prefix[key] || '';
+}
+
+function select(item: any) {
+  navigateTo({ name: pageFunctions.relativeName({ module: 'counterloanfast', realm: 'each', page: 'root' }), params: { id: item.id } });
+}
 </script>
