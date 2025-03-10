97f6f5ef801639a469791a94950615b5aee44623
Jane1m <rinda@finsoft2023.com>
2025-03-10 11:37:30 +0700

    ปรับแต่งตาราง

--- src/modules/mdul_finance/app/pages/finance/list/approve.vue
+++ src/modules/mdul_finance/app/pages/finance/list/approve.vue
@@ -1,17 +1,114 @@
 <template>
   <RealmPageList :pageId>
     <template #default="{ wrapped, entries, resolver }">
+      <div class="flex flex-col items-center space-y-2 mb-4">
+        <h2 class="text-xl font-bold text-center py-2">สหกรณ์ออมทรัพย์โรงพยาบาลศรีเกษ จำกัด</h2>
+        <div class="flex justify-between w-full px-4">
+          <p class="font-normal text-gray-900 dark:text-white">
+            วันที่พิมพ์ :
+          </p>
+          <p class="font-normal text-gray-900 dark:text-white">
+            รายงานการจ่ายเงินกู้
+          </p>
+          <p class="font-normal text-gray-900 dark:text-white">
+            เวลาพิมพ์ :
+          </p>
+        </div>
+        <div class="flex justify-between w-full px-4">
+          <p class="font-normal text-gray-900 dark:text-white">
+            ประเภท :
+          </p>
+          <p class="font-normal text-gray-900 dark:text-white">
+            รายงานการจ่ายเงินกู้ฉุกเฉินวันที่ 10 มีนาคม 2568
+          </p>
+          <p class="font-normal text-gray-900 dark:text-white">
+            ผู้พิมพ์ :
+          </p>
+        </div>
+      </div>
       <EntityTable
         :data="wrapped.data"
+        :ui="{
+          tr: {
+            base: '',
+            selected: 'bg-gray-50 dark:bg-gray-800/50',
+            expanded: 'bg-gray-50 dark:bg-gray-800/50',
+            active: 'hover:bg-gray-50 dark:hover:bg-gray-800/50 cursor-pointer'
+          },
+          th: {
+            base: 'whitespace-nowrap text-center rtl:text-center',
+            padding: 'p-1',
+            color: 'text-gray-900 dark:text-white',
+            font: 'font-semibold',
+            size: 'text-sm'
+          },
+          td: {
+            base: 'whitespace-nowrap text-center rtl:text-center',
+            padding: 'p-1',
+            color: 'text-gray-500 dark:text-gray-400',
+            font: '',
+            size: 'text-sm'
+          },
+        }"
         :columns="columns"
         :entries="entries"
         :resolver="resolver"
-        @selectionChanged="select"
       >
         <template #fullname-data="{ row, column }">
-          {{ getPrefix(row.prefix) }}{{ row.fname }} {{ row.lname }}
+          <div class="text-left">
+            {{ getPrefix(row.prefix) }}{{ row.fname }} {{ row.lname }}
+          </div>
+        </template>
+        <template #amount-data="{ row, column }">
+          <div class="text-right">
+            {{ row.amount }}
+          </div>
+        </template>
+        <template #stockValue-data="{ row, column }">
+          <div class="text-right">
+            {{ row.stockValue }}
+          </div>
+        </template>
+        <template #period-data="{ row, column }">
+          <div class="text-center">
+            {{ row.period }}
+          </div>
+        </template>
+        <template #debt-data="{ row, column }">
+          <div class="text-right">
+            {{ row.debt }}
+          </div>
+        </template>
+        <template #interest-data="{ row, column }">
+          <div class="text-right">
+            {{ row.interest }}
+          </div>
+        </template>
+        <template #balance-data="{ row, column }">
+          <div class="text-right">
+            {{ row.balance }}
+          </div>
+        </template>
+        <template #bankAccount-data="{ row, column }">
+          <div class="text-center">
+            {{ row.bankAccount }}
+          </div>
         </template>
       </EntityTable>
+      <div class="flex justify-between w-full px-4">
+        <p class="font-normal text-gray-900 dark:text-white">
+          รวมหน่วย :
+        </p>
+        <p class="font-normal text-gray-900 dark:text-white">
+        </p>
+      </div>
+      <div class="flex justify-between w-full px-4">
+        <p class="font-normal text-gray-900 dark:text-white">
+          ยอดรวมทั้งสิ้น :
+        </p>
+        <p class="font-normal text-gray-900 dark:text-white">
+        </p>
+      </div>
     </template>
   </RealmPageList>
 </template>
@@ -30,29 +127,42 @@ const pageFunctions = usePageFunctions(pageDef);
 const columns = [
   {
     key: 'no',
-    label: 'ลำดับ',
-  },  
-  // {
-  //   key: 'idmember',
-  //   label: 'รหัสสมาชิก',
-  //   sortable: true
-  // },
+    label: 'ที่',
+  },
+  {
+    key: 'idmember',
+    label: 'เลขสมาชิก',
+  },
   {
     key: 'fullname',
     label: 'ชื่อ-สกุล',
   }, {
-    key: 'position',
-    label: 'ตำแหน่ง',
-  },{
-    key: 'bureau',
-    label: 'หน่วยงาน',
+    key: 'amount',
+    label: 'วงเงินกู้',
+  }, {
+    key: 'stockValue',
+    label: 'หักซื้อหุ้น',
   }, {
     key: 'installments',
-    label: 'จำนวนงวด',
+    label: 'สินเชื่อ',
   }, {
-    key: 'amount',
-    label: 'ยอดที่ต้องการกู้ (บาท)',
-    sortable: true
+    key: 'period ',
+    label: 'งวดที่',
+  }, {
+    key: 'debt',
+    label: 'หนี้คงเหลือ',
+  },
+  {
+    key: 'interest',
+    label: 'ดอกเบี้ย',
+  },
+  {
+    key: 'balance',
+    label: 'คงเหลือรับ',
+  },
+  {
+    key: 'bankAccount',
+    label: 'เลขบัญชี',
   },
 ]
 
@@ -71,7 +181,4 @@ function getPrefix(key: string): string {
   return prefix[key] || '';
 }
 
-function select(item: any) {
-  navigateTo({ name: pageFunctions.relativeName({ module: 'counterloanfast', realm: 'each', page: 'root' }), params: { id: item.id } });
-}
 </script>
