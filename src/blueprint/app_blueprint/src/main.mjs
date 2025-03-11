820af2775629977c67808328c4c9e83872327eef
Jane1m <rinda@finsoft2023.com>
2025-03-11 10:18:34 +0700

    Add navigation link to report page in app modules รวม  UI หน้าโอนเงินของที่ปรับและ qr code เข้าด้วยกัน #63

--- src/blueprint/app_blueprint/src/main.mjs
+++ src/blueprint/app_blueprint/src/main.mjs
@@ -1192,6 +1192,7 @@ export const appModules = defineAppModules({
           },        
           {
             label: 'ส่งเรื่องให้บัญชี',
+            to: { page: 'report' },
           }         
         ],
       },
