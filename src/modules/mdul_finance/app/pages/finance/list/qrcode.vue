e6ee77e0feae6d88ad68862522997abc386b674d
Jane1m <rinda@finsoft2023.com>
2025-03-11 10:17:08 +0700

    Refactor navigation and update QR Code link for money transfer functionality รวม  UI หน้าโอนเงินของที่ปรับและ qr code เข้าด้วยกัน #63

--- src/modules/mdul_finance/app/pages/finance/list/qrcode.vue
+++ src/modules/mdul_finance/app/pages/finance/list/qrcode.vue
@@ -3,10 +3,12 @@
     <template #default="{ wrapped }">
       <h1 class="text-center font-bold">QR Code โอนเงิน</h1>
       <div class="flex justify-center">
-        <FieldViewQrCode
-          modelValue="link_your_website_here/officerapp/list/complete"
-          style="width: 300px; height: 300px;"
-        ></FieldViewQrCode>
+        <NuxtLink to="/finance/list/completed">
+          <FieldViewQrCode
+            modelValue="link_your_website_here/officerapp/list/complete"
+            style="width: 300px; height: 300px;"
+          ></FieldViewQrCode>
+        </NuxtLink>
       </div>
       <li v-for="(msg, i) in messages">{{ msg.data }}</li>
     </template>
