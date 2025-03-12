66fa32450a073fdcbfe2d00eb01f707751a8b1f0
AhLei <138641969+9Kittipich@users.noreply.github.com>
2025-03-12 10:35:16 +0700

    Refactor navigation links and enhance officer app completion page with confirmation message and button

--- src/modules/mdul_officerapp/app/pages/officerapp/list/complete.vue
+++ src/modules/mdul_officerapp/app/pages/officerapp/list/complete.vue
@@ -1,5 +1,28 @@
 <template>
-  <RealmPageList :pageId></RealmPageList>
+  <div class="mx-auto max-w-screen-sm text-center">
+        <div>
+          <h3 class="text-2xl font-bold dark:text-white">
+            ยืนยันตัวตนเรียบร้อยแล้ว
+          </h3>
+          <p class="text-lg text-gray-500 dark:text-gray-400 mt-4">
+            กรุณากดปุ่ม "ยืนยันตัวตนเสร็จสิ้น" เพื่อสิ้นสุดขั้นตอนการยืนยันตัวตน
+          </p>
+          <div class="flex justify-center mt-6">
+            <!-- <FieldViewQrCode modelValue="กรุณาเปิดกล้องโทรศัทพ์ของคุณในการสแกน" class="w-[150px] h-[150px]" ></FieldViewQrCode> -->
+            <UIcon name="fluent:person-link-20-filled" size="150"></UIcon>
+          </div>
+        </div>
+        <div>
+          <!-- <UButton class="mt-10"
+            icon="i-heroicons-arrow-path-solid"
+            color="gray"
+            variant="outline">Refresh</UButton> -->
+
+          <div class="mt-10 flex justify-center"></div>
+          <UButton @click="sendMessage">ยืนยันตัวตนเสร็จสิ้น</UButton>
+          <li v-for="(msg, i) in messages">{{ msg }}</li>
+        </div>
+      </div>
 </template>
 
 <script setup lang="ts">
@@ -9,4 +32,18 @@ definePageMeta({
 const pageId = {
   page: 'complete',
 };
+
+const { messageReceived, publish } = useLiveSync("greetings", "verify");
+const messages = ref<any[]>([]);
+const message = ref<string>("Verify Success");
+
+watch(messageReceived, (newMessages) => {
+  messages.value.push(newMessages);
+});
+
+function sendMessage() {
+  publish(message.value);
+  // navigateTo({ name: 'officerapp.list.root' }); 
+  window.close();
+}
 </script>
