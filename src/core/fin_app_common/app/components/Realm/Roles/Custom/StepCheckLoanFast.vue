d7773731e2117e1cb86fdc1b5a9fb1bf706fa9a9
AhLei <138641969+9Kittipich@users.noreply.github.com>
2025-03-13 10:17:30 +0700

    Update StepCheckLoanFast.vue

--- src/core/fin_app_common/app/components/Realm/Roles/Custom/StepCheckLoanFast.vue
+++ src/core/fin_app_common/app/components/Realm/Roles/Custom/StepCheckLoanFast.vue
@@ -55,10 +55,11 @@ const { pageDef, pageFunctions, title, actions, data, status, error, saveThenGoN
 const resultMessage = ref<string>('');
 
 const maxLoanLimits: Record<string, number> = {
-  option1: 200000,
-  option2: 50000,
-  option3: 100000,
-  option4: 100000
+    option1: 200000,
+    option2: 100000,
+    option3: 50000,
+    option4: 100000,
+    option5: 100000,
 };
 
 const maxLoanAmount = computed(() => {
