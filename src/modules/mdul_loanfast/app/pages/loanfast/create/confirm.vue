b6e87a53f0367cba39efefed55c2e3678287cf2c
baramee007 <nobpharut0191@finsoft2023.com>
2025-03-20 12:20:40 +0700

    Refactor loanfast module: update role and labels, and enhance template structure

--- src/modules/mdul_loanfast/app/pages/loanfast/create/confirm.vue
+++ src/modules/mdul_loanfast/app/pages/loanfast/create/confirm.vue
@@ -1,5 +1,15 @@
 <template>
-  <RealmPageCreate :pageId></RealmPageCreate>
+  <RealmPageCreate :pageId>
+    <template #default="{ wrapped, entries, resolver }">
+      <EntitySection v-model="wrapped.data"
+        :entries
+        :resolver></EntitySection>
+      <p class="mt-4 text-xl font-bold">ตารางส่งเงินคืน 12 งวด</p>
+      <FLoanTable :months="12"
+        :interest-rate="5.59"
+        :loan-amount="wrapped.data.amount" />
+    </template>
+  </RealmPageCreate>
 </template>
 
 <script setup lang="ts">
